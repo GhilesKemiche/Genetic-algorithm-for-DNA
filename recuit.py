bbcef0ad2b876b8f69805b1518105c9f2083a503
Maxime Pilorget <maxime.pilorget@student-cs.fr>
2024-01-29 16:15:09 +0100

    creation

---
+++ recuit.py
@@ -0,0 +1,27 @@
+class Recuit:
+    def __init__(s,k_max,e_min):
+        self.s=s
+        self.k_max=k_max
+        self.e_min=e_min
+        
+    @classmethod
+    def optimization_state(s0,k_max,e_min):
+        
+        
+    @classmethod
+    def initial_state():
+        
+    @classmethod
+    def cost():
+        
+    @classmethod
+    def neighbor():
+        
+    @classmethod
+    def probability():
+        
+    @classmethod
+    def curbs()
+        
+    
+        
