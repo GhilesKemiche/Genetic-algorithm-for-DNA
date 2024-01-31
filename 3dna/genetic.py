0ba4707e601b78257ff63beac152e33ec18df143
Kalla Mehdi <mehdi.kalla@student-cs.fr>
2024-01-31 08:26:58 +0100

    "Modif genetic et utilitaires"

--- 3dna/genetic.py
+++ 3dna/genetic.py
@@ -8,6 +8,7 @@ from .Traj3D import Traj3D
 import copy as cp
 
 
+
 def generate_rotTable():
   random_rotTable = RotTable()
 
@@ -28,8 +29,8 @@ class individu:
 
   def encode_chromosomes(self):
     for dinucleotide in rotTable.getTable().keys():
-      self.chromosome_twist[dinucleotide] = bin(rotTable.getTwist[dinucleotide])
-      self.chromosome_wedge[dinucleotide] = bin(rotTable.getWedge[dinucleotide])
+      self.chromosome_twist[dinucleotide] = dec_to_bin(rotTable.getTwist[dinucleotide])
+      self.chromosome_wedge[dinucleotide] = dec_to_bin(rotTable.getWedge[dinucleotide])
 
   def mutate(self):
     L = len(chromosome)
@@ -74,61 +75,3 @@ class genetic:
       self.mutation.append(i.mutate())
     return self.mutation
 
-
-
-
-def dec_to_bin(number, places=15):
-  whole, dec = str(number).split(".")
-  whole = int(whole)
-  dec = int(dec)
-  res = bin(whole).lstrip("0b") + "."
-  for x in range(places):
-    whole, dec = str((decimal_converter(dec)) * 2).split(".")
-    dec = int(dec)
-    res += whole
-  return float(res)
-
-def decimal_converter(num):
-  while(num>1):
-    num /= 10
-  return num
-
-def bin_to_dec(binary):
-  binary = str(binary)
-  whole_str, *dec_str = binary.split('.')
-
-  whole_dec = int(whole_str, 2)
-  dec_dec = 0.
-
-  if dec_str:
-    for k in range(len(dec_str[0])):
-      if(dec_str[0][k] == '1'):
-        dec_dec = dec_dec + float(2**(-k-1))
-  else:
-    dec_dec = 0
-
-  return whole_dec + dec_dec
-
-
-# Call the function as : quicksort(A,0,len(A)-1)
-def quicksort(A, lo, hi):
-  if lo >= hi or lo < 0:
-    return
-
-  p = partition(A, lo, hi)
-
-  quicksort(A, lo, p - 1)
-  quicksort(A, p + 1, hi)
-
-def partition(A, lo, hi):
-  pivot = A[hi]
-  i = lo - 1
-
-  for j in range(lo,hi):
-    if A[j]<=pivot:
-      i = i + 1
-      A[i], A[j] = A[j], A[i]
-
-  i = i + 1
-  A[i], A[hi] = A[hi], A[i]
-  return i
