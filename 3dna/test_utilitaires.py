da91aec7c11011f9f993dc58423d6b36b01644d0
Kalla Mehdi <mehdi.kalla@student-cs.fr>
2024-01-31 14:48:16 +0100

    "Test utilitaires"

---
+++ 3dna/test_utilitaires.py
@@ -0,0 +1,40 @@
+from utilitaires import*
+from pytest import*
+
+### BATTERIE DE TEST POUR utilitaires.py ###
+
+### Il suffit de RUN le fichier ###
+
+def test_dec_to_bin():
+    pass
+
+def test_decimal_converter():
+    pass
+
+def test_bin_to_dec():
+    pass
+
+def test_quicksort():
+    pass
+
+def test_partition():
+    pass
+
+def test_list_to_str():
+    pass
+
+def test_decompose_dict_list():
+    pass
+
+def test_resize_bin():
+    pass
+
+def test_change_str():
+    pass
+
+def test_merge_dict():
+    pass
+
+def test_back_to_dec():
+    pass
+
