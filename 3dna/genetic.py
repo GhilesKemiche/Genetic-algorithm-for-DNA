4477ece7687ba1e7307a7eee7cd8fdbdf6419bf0
Maxime Pilorget <maxime.pilorget@student-cs.fr>
2024-02-01 15:51:41 +0100

    readme, /seaborn

--- 3dna/genetic.py
+++ 3dna/genetic.py
@@ -1,6 +1,5 @@
 import random
 from .RotTable import RotTable
-import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
@@ -268,5 +267,4 @@ def initialisation(n):
 
 
         
-
 
