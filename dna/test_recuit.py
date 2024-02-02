3c6e4f4bf8fa57dce5ac8a90f2358605bdef4e2b
Maxime Pilorget <maxime.pilorget@student-cs.fr>
2024-02-02 09:34:28 +0100

    ok

--- dna/test_recuit.py
+++ dna/test_recuit.py
@@ -1,13 +1,12 @@
 import unittest
-<<<<<<< HEAD
-from .recuit import Recuit  
-from .RotTable import RotTable
-from .Traj3D import Traj3D
+from dna.recuit import Recuit  
+from dna.RotTable import RotTable
+from dna.Traj3D import Traj3D
 
 ### BATTERIE DE TEST POUR recuit.py ###
 
-### RUN python -m unittest test_recuit.py ###
-### Exemple: en se plaçant dans Genetic-algorithm-for-DNA, entrer: python -m unittest 3dna/test_recuit.py ###
+### RUN python -m pytest dna/test_recuit.py ###
+
 
 
 
@@ -55,65 +54,4 @@ class TestRecuit(unittest.TestCase):
         self.assertIsInstance(result, float, "La méthode temp ne renvoie pas un nombre décimal.")
 
 if __name__ == '__main__':
-=======
-import numpy as np
-from dna.recuit import * 
-from dna.RotTable import RotTable
-
-### RUN python -m pytest dna/test_recuit.py ###
-
-class TestRecuit(unittest.TestCase):
-    """
-    Classe TestRecuit pour tester les différentes méthodes de la classe Recuit.
-    """
-
-    def setUp(self):
-        """
-        Méthode appelée avant chaque test pour configurer l'environnement de test. 
-        Initialise une instance de Recuit et RotTable, ainsi que les paramètres de base.
-        """
-        self.rotTable = RotTable()  
-        self.recuit = Recuit(self.rotTable)
-        self.dna_seq = "ATCG"  
-        self.init_temp = 1
-        self.distance_weight = 0.1
-        self.alignment_weight = 10.0
-
-    def test_probability(self):
-        """
-        Teste la méthode probability de la classe Recuit.
-        Vérifie que la probabilité calculée est égale à la valeur attendue.
-        """
-        dE = -1
-        temp = 1
-        expected_prob = np.exp(-dE / temp)
-        result_prob = self.recuit.probability(dE, temp)
-        self.assertAlmostEqual(result_prob, expected_prob, msg="La fonction probability ne retourne pas la valeur attendue.")
-
-    def test_temp(self):
-        """
-        Teste la méthode temp de la classe Recuit.
-        Vérifie que la température calculée est égale à la valeur attendue.
-        """
-        t = 0.5
-        t0 = 40
-        expected_temp = t0 * (1 - t)
-        result_temp = self.recuit.temp(t, t0)
-        self.assertEqual(result_temp, expected_temp, msg="La fonction temp ne retourne pas la valeur attendue.")
-
-    def test_cost(self):
-        """
-        Teste la méthode cost de la classe Recuit.
-        Vérifie que le coût total et ses composants (distance, alignement, twist, wedge) sont égaux aux valeurs attendues.
-        """
-        total_cost, distance_cost, alignment_cost, cost_twist_complement, cost_wedge_complement = self.recuit.cost(self.dna_seq, self.rotTable, self.distance_weight, self.alignment_weight)
-
-        self.assertAlmostEqual(total_cost, 37.87999244135347)
-        self.assertAlmostEqual(distance_cost, 10.125422644694387)
-        self.assertAlmostEqual(alignment_cost,  3.686745017688404)
-        self.assertAlmostEqual(cost_twist_complement, 0.0)
-        self.assertAlmostEqual(cost_wedge_complement, 0.0)
-
-if __name__ == '_main_':
->>>>>>> a5b35a9d35a761f2aee8882a93e0a6a24b48752f
     unittest.main()
