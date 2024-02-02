cabe727d3f02c09bad8e0f7d2da79dbd821181a4
Subramaniam Abiram <abiram.subramaniam@student-cs.fr>
2024-02-02 09:17:26 +0100

    test_genetic.py

--- dna/test_genetic.py
+++ dna/test_genetic.py
@@ -1,13 +1,12 @@
 import unittest
-from .genetic import *  
-from .RotTable import RotTable
-from .Traj3D import Traj3D
+from dna.genetic import *  
+from dna.RotTable import RotTable
+from dna.Traj3D import Traj3D
 from copy import deepcopy
 
 ### BATTERIE DE TEST POUR genetic.py ###
 
-### RUN python -m unittest test_genetic.py ###
-### Exemple: en se plaçant dans Genetic-algorithm-for-DNA, entrer: python -m unittest 3dna/test_genetic.py ###
+### RUN python -m pytest 3dna/test_genetic.py ###
 
 class TestGenetic(unittest.TestCase):
 
@@ -34,15 +33,17 @@ class TestGenetic(unittest.TestCase):
     def test_encoding_probas(self):
         # Test encoding probabilities for an individual
         ind = individu()
-        cop=cp.deepcopy(ind.encode_probas())
-        self.assertNotEqual(ind,cop,"aucune modif")
+        if len(decompose_dict_list(ind.chromosome_twist)) != 0:
+            cop=cp.deepcopy(ind.encode_probas())
+            self.assertNotEqual(ind,cop,"aucune modif")
 
     def test_mutate(self):
         # Test mutation of an individual
         ind = individu()
-        cop=cp.deepcopy(ind.encode_probas())
-        ind.mutate()
-        self.assertNotEqual(ind,cop,"aucune modif")
+        if len(decompose_dict_list(ind.chromosome_twist)) != 0:
+            cop=cp.deepcopy(ind.encode_probas())
+            ind.mutate()
+            self.assertNotEqual(ind,cop,"aucune modif")
 
     def test_evaluation(self):
         # Test the evaluation function in the genetic class
@@ -50,14 +51,14 @@ class TestGenetic(unittest.TestCase):
         population = initialisation(10)
         gen_alg = genetic(population)
         evaluation = gen_alg.do_evaluation(dna_seq)
-        self.assertIsInstance(float,"erreur de typage")
+        self.assertIsInstance(evaluation, dict,"erreur de typage")
 
     def test_selection(self):
         # Test the selection function in the genetic class
         population = initialisation(10)
         gen_alg = genetic(population)
         gen_alg.do_evaluation("ATCG")
-        selection = gen_alg.do_selection()
+        selection = gen_alg.do_selection(1)
         self.assertEqual(len(selection),len(population)//2,"la population n'est pas divisée par 2")
 
     def test_crossover(self):
@@ -65,16 +66,16 @@ class TestGenetic(unittest.TestCase):
         population = initialisation(10)
         gen_alg = genetic(population)
         gen_alg.do_evaluation("ATCG")
-        gen_alg.do_selection()
+        gen_alg.do_selection(1)
         crossover = gen_alg.do_croisement()
-        self.assertIsInstance(list,"non type")
+        self.assertIsInstance(crossover, list,"non type")
         
     def test_mutation(self):
         # Test the mutation function in the genetic class
         population = initialisation(10)
         gen_alg = genetic(population)
         gen_alg.do_evaluation("ATCG")
-        gen_alg.do_selection()
+        gen_alg.do_selection(1)
         gen_alg.do_croisement()
         mutation = gen_alg.do_mutation()
         self.assertEqual(len(mutation),len(population),"nb different d'individus")
