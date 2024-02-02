23f236ed470b46c26474fe69cc1433fa3a5f23b4
GhilesKemiche <97968146+GhilesKemiche@users.noreply.github.com>
2024-02-02 09:27:57 +0100

    Update __main__.py

--- dna/__main__.py
+++ dna/__main__.py
@@ -2,9 +2,7 @@ from .RotTable import RotTable
 from .Traj3D import Traj3D
 from .recuit import Recuit
 import numpy as np
-from .genetic import *
-
-
+import time
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="input filename of DNA sequence")
@@ -12,48 +10,59 @@ parser.parse_args()
 args = parser.parse_args()
 
 def main():
+    start_time = time.time()
 
+    
     rot_table = RotTable()
     
     # Read file
     lineList = [line.rstrip('\n') for line in open(args.filename)]
     # Formatting
     seq = ''.join(lineList[1:])
-    
-    
+
+
     n=100
     k=50
-    
     algo=genetic(initialisation(n))
     algo.algo_gen(k,seq)
     
+    # if len(seq) > 10000 :
+    #     distance_weight = 0.12
+    #     alignment_weight = 10
+    #     init_temp = 50.0
+    #     max_iter = 10000 
+    #     cost_min = 50
+    #     twist_weight = 30
+    #     wedge_weight = 100
+    # else : 
+    #     distance_weight = 1.2
+    #     alignment_weight = 50
+    #     init_temp = 1.0
+    #     max_iter = 15000 
+    #     cost_min = 50
+    #     twist_weight = 300
+    #     wedge_weight = 1000
     
-    
+    # np.random.seed(seed = 750)
+    # recuit=Recuit(rot_table)
+    # rot_table_opt, traj = recuit.optimization_state(seq, max_iter= max_iter, cost_min= cost_min,\
+    #                                                  init_temp= init_temp, distance_weight= distance_weight,\
+    #                                                       alignment_weight= alignment_weight, twist_weight = twist_weight, wedge_weight = wedge_weight)
+    # traj.compute(seq, rot_table_opt)
+    # # print(traj.getTraj())
+    # print('la table de rotation optimale est : \n', rot_table_opt.getTable())
+    # traj.draw()
+    # traj.write(args.filename+".png")
 
-    
-    
 
-    
 
-    
-    """
-    seq = ''.join(lineList[1:])
-    distance_weight = 1.0
-    alignment_weight = 50
-    init_temp = 1.0
-    max_iter = 14000 
-    cost_min = 5
-    np.random.seed(seed = 750)
-    recuit=Recuit(rot_table)
-    rot_table_opt, traj = recuit.optimization_state(seq, max_iter= max_iter, cost_min= cost_min, init_temp= init_temp, distance_weight= distance_weight, alignment_weight= alignment_weight)
-    traj.compute(seq, rot_table_opt)
-    """
-    
-    #print(traj.getTraj())
-    #traj.draw()
-    #traj.write(args.filename+".png")
-    #print(rot_table_opt)
-    
+
+
+    end_time = time.time()
+    print()
+    print(f"Temps d'exécution : {end_time - start_time} secondes")
+
+
 
 if __name__ == "__main__" :
     main()
