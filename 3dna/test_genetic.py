6830dd26bd8893cee4a3cd8707bf0f9005e886d0
Maxime Pilorget <maxime.pilorget@student-cs.fr>
2024-02-02 01:43:34 +0100

    ok

--- 3dna/test_genetic.py
+++ 3dna/test_genetic.py
@@ -2,6 +2,7 @@ import unittest
 from .genetic import *  
 from .RotTable import RotTable
 from .Traj3D import Traj3D
+from copy import deepcopy
 
 ### BATTERIE DE TEST POUR genetic.py ###
 
@@ -26,21 +27,22 @@ class TestGenetic(unittest.TestCase):
     def test_encoding_chromosomes(self):
         # Test encoding chromosomes for an individual
         ind = individu()
-        ind.encode_chromosomes()
-        # Add more specific assertions based on your requirements
+        cop=cp.deepcopy(ind.encode_chromosomes())
+        self.assertNotEqual(ind,cop,"aucune modif")
+        
 
     def test_encoding_probas(self):
         # Test encoding probabilities for an individual
         ind = individu()
-        ind.encode_probas()
-        # Add more specific assertions based on your requirements
+        cop=cp.deepcopy(ind.encode_probas())
+        self.assertNotEqual(ind,cop,"aucune modif")
 
     def test_mutate(self):
         # Test mutation of an individual
         ind = individu()
-        ind.encode_probas()
+        cop=cp.deepcopy(ind.encode_probas())
         ind.mutate()
-        # Add more specific assertions based on your requirements
+        self.assertNotEqual(ind,cop,"aucune modif")
 
     def test_evaluation(self):
         # Test the evaluation function in the genetic class
@@ -48,7 +50,7 @@ class TestGenetic(unittest.TestCase):
         population = initialisation(10)
         gen_alg = genetic(population)
         evaluation = gen_alg.do_evaluation(dna_seq)
-        # Add more specific assertions based on your requirements
+        self.assertIsInstance(float,"erreur de typage")
 
     def test_selection(self):
         # Test the selection function in the genetic class
@@ -56,7 +58,7 @@ class TestGenetic(unittest.TestCase):
         gen_alg = genetic(population)
         gen_alg.do_evaluation("ATCG")
         selection = gen_alg.do_selection()
-        # Add more specific assertions based on your requirements
+        self.assertEqual(len(selection),len(population)//2,"la population n'est pas divisée par 2")
 
     def test_crossover(self):
         # Test the crossover function in the genetic class
@@ -65,8 +67,8 @@ class TestGenetic(unittest.TestCase):
         gen_alg.do_evaluation("ATCG")
         gen_alg.do_selection()
         crossover = gen_alg.do_croisement()
-        # Add more specific assertions based on your requirements
-
+        self.assertIsInstance(list,"non type")
+        
     def test_mutation(self):
         # Test the mutation function in the genetic class
         population = initialisation(10)
@@ -75,15 +77,15 @@ class TestGenetic(unittest.TestCase):
         gen_alg.do_selection()
         gen_alg.do_croisement()
         mutation = gen_alg.do_mutation()
-        # Add more specific assertions based on your requirements
-
+        self.assertEqual(len(mutation),len(population),"nb different d'individus")
+        
     def test_algorithm(self):
         # Test the entire genetic algorithm
         dna_seq = "ATCG"
         population = initialisation(10)
         gen_alg = genetic(population)
         gen_alg.algo_gen(5, dna_seq)
-        # Add more specific assertions based on your requirements
+        self.assertIsNotNone(gen_alg,"erreur typage")
 
 
 
